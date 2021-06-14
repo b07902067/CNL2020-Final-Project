bfbd755298ddfc9490f2be0789146f0fbf6ffc83
b07902067 <b07902067@ntu.edu.tw>
2021-06-14 16:30:05 +0800

    modify : AP.py

--- AP/AP.py
+++ AP/AP.py
@@ -33,7 +33,7 @@ def sendqueue(queue, IP):
 	server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 	server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
     # server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+	# server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 	server.settimeout(0.2)
 	print("sned queue message is : ", send_ID, "  to this IP : ", IP)
 	message = str.encode(send_ID)
